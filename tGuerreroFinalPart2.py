11cea5149398566424831358e57f96414a469f70
tedguerrero <tedguerrerojr@gmail.com>
2026-01-29 01:57:02 +0000

    Part 2 of the Final

---
+++ tGuerreroFinalPart2.py
@@ -0,0 +1,11 @@
+#Ted Guerrero
+#Final Part 2
+
+#Function definitions
+def holdInteger(holding):
+    for i in range(holding):
+        print("Happy New Year!")
+
+entry = int(input("Enter a number between 1 and 10: "))
+holdInteger(entry)
+
